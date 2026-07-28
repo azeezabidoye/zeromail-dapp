4f67be2a2d7b71776dcff150bc4470658751f96e
azeezabidoye <azeezabidoye@yahoo.com>
2026-07-28 18:29:49 +0100

    feat: Deploy Script Updated

--- tasks/deploy-counter.ts
+++ tasks/deploy-counter.ts
@@ -8,23 +8,4 @@ task(
   "Deploy the Counter contract to the selected network",
 ).setAction(async (_, hre: HardhatRuntimeEnvironment) => {
   const { ethers, network } = hre;
-
-  // console.log(`Deploying Counter to ${network.name}...`)
-
-  // // Get the deployer account
-  // const [deployer] = await ethers.getSigners()
-  // console.log(`Deploying with account: ${deployer.address}`)
-
-  // // Deploy the contract
-  // const Counter = await ethers.getContractFactory('Counter')
-  // const counter = await Counter.deploy()
-  // await counter.waitForDeployment()
-
-  // const counterAddress = await counter.getAddress()
-  // console.log(`Counter deployed to: ${counterAddress}`)
-
-  // // Save the deployment
-  // saveDeployment(network.name, 'Counter', counterAddress)
-
-  // return counterAddress
 });
